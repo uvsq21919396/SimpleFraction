ae164951566a36035898a1da07e351b2014e648a
Koceila LABCHRI <koceila.labchri@ens.uvsq.fr>
2020-09-28 15:53:13 +0200

    TD1 : ajout des constucteurs et de la methode toString testé

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,43 @@
 
 public class Fraction {
+	private int numerateur;
+	private int denominateur;
+	
+	public Fraction() {
+		numerateur =0;
+		denominateur =1;
+	}
+	
+	public Fraction(int numerateur, int denumerateur) {
+		this.numerateur = numerateur;
+		this.denominateur=denumerateur;
+	}
+
+	public int getNumerateur() {
+		return numerateur;
+	}
+
+	public void setNumerateur(int numerateur) {
+		this.numerateur = numerateur;
+	}
+
+	public int getDenumerateur() {
+		return denominateur;
+	}
+
+	public void setDenumerateur(int denumerateur) {
+		this.denominateur = denumerateur;
+	}
+	
+	@Override
+	public String toString() {
+		return "Fraction [numerateur=" + numerateur + ", denominateur=" + denominateur + "]";
+	}
+	
+	public double valeurReelle() {
+		return (double)this.numerateur / this.denominateur;
+	}
+	
+	
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world !");
+		Fraction f1 = new Fraction(4,5);
+		Fraction f2 = new Fraction(3,2);
+		System.out.println(f1);
+		
+		System.out.println(f2.toString());
+		System.out.println("Valeur reelle de F2 ="+f2.valeurReelle());
 	}
 
 }
